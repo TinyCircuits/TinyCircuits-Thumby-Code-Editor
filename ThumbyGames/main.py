b4bac79d006c8b80526a65e3bc07946243b2bcab
Andrew Dent <dent.ace@gmail.com>
2022-12-15 13:02:48 +0000

    Remove hardware ID from `main.py` bootloader
    - Remove HWID and do minimum required to display splash screen.
    - Avoids duplicate code in `thumbyHardware.py` (HWID is already determined by thumby lib, executed from menu.py).
    - Remove import of `machine.Timer` - unused.

--- ThumbyGames/main.py
+++ ThumbyGames/main.py
@@ -1,5 +1,5 @@
 # Thumby main.py- quick initialization to display the TinyCircuits logo before menu.py is called
-# Last updated 14-Dec-2022
+# Last updated 15-Dec-2022
 
 from machine import mem32, freq, reset
 freq(133_000_000)
@@ -23,15 +23,13 @@ if(mem32[SCRATCH0_ADDR]==1):
         from time import sleep_ms
         print("\nThis Thumby script crashed... :(")
         print_exception(e)
-        
-        # Let the exception print before resetting
         sleep_ms(1000)
         reset()
     else:
         reset()
 
 
-from machine import Pin, Timer, I2C, SPI
+from machine import Pin, I2C, SPI
 import ssd1306
 
 
@@ -46,34 +44,15 @@ except OSError:
 
 brightnessVals=[0,28,127]
 
-HWID = 0
+
 IDPin = Pin(15, Pin.IN, Pin.PULL_UP)
 if(IDPin.value() == 0):
-    HWID+=1
-IDPin = Pin(14, Pin.IN, Pin.PULL_UP)
-if(IDPin.value() == 0):
-    HWID+=2
-IDPin = Pin(13, Pin.IN, Pin.PULL_UP)
-if(IDPin.value() == 0):
-    HWID+=4
-IDPin = Pin(12, Pin.IN, Pin.PULL_UP)
-if(IDPin.value() == 0):
-    HWID+=8
-IDPin = Pin(15, Pin.IN, Pin.PULL_DOWN)
-IDPin = Pin(14, Pin.IN, Pin.PULL_DOWN)
-IDPin = Pin(13, Pin.IN, Pin.PULL_DOWN)
-IDPin = Pin(12, Pin.IN, Pin.PULL_DOWN)
-
-
-i2c = None
-spi = None
-display=None
-if(HWID==0):
-    i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=1000000)
-    display = ssd1306.SSD1306_I2C(72, 40, i2c, res=Pin(18))
-if(HWID>=1):
-    spi = SPI(0, sck=Pin(18), mosi=Pin(19)) #possible assignment of miso to 4 or 16?
+    spi = SPI(0, sck=Pin(18), mosi=Pin(19)) # Assign miso to 4 or 16?
     display = ssd1306.SSD1306_SPI(72, 40, spi, dc=Pin(17), res=Pin(20), cs=Pin(16))
+else:
+    i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=1_000_000)
+    display = ssd1306.SSD1306_I2C(72, 40, i2c, res=Pin(18))
+IDPin = Pin(15, Pin.IN, Pin.PULL_DOWN)
 
 display.init_display()
 display.contrast(brightnessVals[brightnessSetting])
@@ -85,5 +64,6 @@ display.show()
 
 
 import menu
+
 
 reset()
