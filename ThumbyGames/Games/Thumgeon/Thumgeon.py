7e0e252622e1e2fe935b77e72332254d3a1e5ee0
Andrew Dent <dent.ace@gmail.com>
2023-08-07 18:31:49 +0100

    Tidy up Thumgeon
    - Remove unused module handling code.

--- ThumbyGames/Games/Thumgeon/Thumgeon.py
+++ ThumbyGames/Games/Thumgeon/Thumgeon.py
@@ -1674,42 +1674,6 @@ while(True):
     while(swBstate == 1):
         getcharinputNew()
 
-    def delete_module(modname, paranoid=None):
-        from sys import modules
-        try:
-            thismod = modules[modname]
-        except KeyError:
-            raise ValueError(modname)
-        these_symbols = dir(thismod)
-        if paranoid:
-            try:
-                paranoid[:]  # sequence support
-            except:
-                raise ValueError('must supply a finite list for paranoid')
-            else:
-                these_symbols = paranoid[:]
-        del modules[modname]
-        for mod in modules.values():
-            try:
-                delattr(mod, modname)
-            except AttributeError:
-                pass
-            if paranoid:
-                for symbol in these_symbols:
-                    if symbol[:2] == '__':  # ignore special symbols
-                        continue
-                    try:
-                        delattr(mod, symbol)
-                    except AttributeError:
-                        pass
-
-    def reload(mod):
-        import sys
-        if mod in sys.modules:
-            del sys.modules[mod]
-            gc.collect()
-        __import__(mod)
-
     while(swBstate != 1):
         thumby.display.fill(0)
         thumby.display.drawText("Restart?", 0, 8, 1)
